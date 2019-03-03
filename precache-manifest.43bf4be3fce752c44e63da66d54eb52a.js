a412f8e232e9a23a5f00783ebb25a1e55aae6502
kiwidamien <djmartin@ucdavis.edu>
2019-03-02 16:05:10 -0800

    Updates

---
+++ precache-manifest.43bf4be3fce752c44e63da66d54eb52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c889292074e8690a6dd4",
+    "url": "/d3_roc/static/js/runtime~main.c8892920.js"
+  },
+  {
+    "revision": "30d6971048c4346c5163",
+    "url": "/d3_roc/static/js/main.30d69710.chunk.js"
+  },
+  {
+    "revision": "415d1cc3ab43acc3a26e",
+    "url": "/d3_roc/static/js/2.415d1cc3.chunk.js"
+  },
+  {
+    "revision": "30d6971048c4346c5163",
+    "url": "/d3_roc/static/css/main.2c390896.chunk.css"
+  },
+  {
+    "revision": "7f55cd5951db612c723ad359f96cf625",
+    "url": "/d3_roc/index.html"
+  }
+];
