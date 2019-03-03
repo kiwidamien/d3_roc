f3df303a48299a42ea25f72de3296aa9f5ca9e36
kiwidamien <djmartin@ucdavis.edu>
2019-03-02 16:42:53 -0800

    added legend

--- src/LabelReporter.js
+++ src/LabelReporter.js
@@ -6,6 +6,8 @@ const LabelReporter = (props) => {
     <div>
     <div className='title'>Threshold: {props.threshold}</div>
     <div className='subtitle'>i.e. accept everyone with credit scores above {props.threshold}</div>
+    <div className='subtitle'>People shown in <b><span style={{color: 'red'}}>red</span></b> actually defaulted</div>
+    <div className='subtitle'>People shown in <b><span style={{color: 'black'}}>black</span></b> actually repaid</div>
     </div>
   </div>
   );
